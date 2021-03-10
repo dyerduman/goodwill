f37b0b6af7f4ef0f3b1d91c60da364b7af8e5846
dyerduman <hello@dyerdumandesign.ca>
2021-03-10 11:29:59 -0500

    Reordering quicklinks and primary navigation (to ensure correct tab index order)

--- wp-content/themes/twentytwentyone-child/template-parts/header/site-nav.php
+++ wp-content/themes/twentytwentyone-child/template-parts/header/site-nav.php
@@ -11,19 +11,6 @@
 
 <?php if ( has_nav_menu( 'primary' ) ) : ?>
 	<nav id="site-navigation" class="primary-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'twentytwentyone' ); ?>">
-		<!--<div class="quicklinks">
-			<ul>
-		<?php
-
-					wp_nav_menu(
-						array(
-							'container'  => '',
-							'items_wrap' => '%3$s',
-							'theme_location' => 'tertiary',
-						)
-					); ?>
-				</ul>
-			</div>-->
 		<div class="menu-button-container">
 			<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
 				<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'twentytwentyone' ); ?>
@@ -38,20 +25,20 @@
 		<?php
 		wp_nav_menu(
 			array(
-				'theme_location'  => 'primary',
+				'theme_location'  => 'tertiary',
 				'menu_class'      => 'menu-wrapper',
-				'container_class' => 'primary-menu-container',
-				'items_wrap'      => '
-				<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+				'container_class' => 'quicklinks',
+				'items_wrap'      => '<ul>%3$s</ul>',
 				'fallback_cb'     => false,
 			)
 		);
 		wp_nav_menu(
 			array(
-				'theme_location'  => 'tertiary',
+				'theme_location'  => 'primary',
 				'menu_class'      => 'menu-wrapper',
-				'container_class' => 'quicklinks',
-				'items_wrap'      => '<ul>%3$s</ul>',
+				'container_class' => 'primary-menu-container',
+				'items_wrap'      => '
+				<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
 				'fallback_cb'     => false,
 			)
 		);
