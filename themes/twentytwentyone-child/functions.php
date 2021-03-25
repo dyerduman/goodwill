2fd691d71a4752b6cc14608b5bfb28020bcbb905
dyerduman <hello@dyerdumandesign.ca>
2021-03-25 16:53:38 -0400

    Updating posts archive structure and styles

--- themes/twentytwentyone-child/functions.php
+++ themes/twentytwentyone-child/functions.php
@@ -22,22 +22,19 @@ return wp_nav_menu( array( 'menu' => $name, 'menu_class' => 'myclass', 'echo' =>
 
 add_shortcode('menu', 'print_menu_shortcode');
 
-/*attempting to extend blocks with AOS data attribute
 
-const { addFilter } = wp.hooks;
-
-ffunction addAttributes( extraProps, blockType, attributes ) {
-if(blockType.name !== 'core/group') {
-return extraProps;
-}
-extraProps['data-aos'] = 'fade-up';
-extraProps['data-aos-duration'] = '1500';
-
-return extraProps;
-}
-
-addFilter(
-'blocks.getSaveContent.extraProps',
-'test/applyAOSClasses',
-addAttributes
-);
+/*removing "category" from archive title*/
+add_filter( 'get_the_archive_title', function ($title) {
+        if ( is_category() ) {
+                $title = single_cat_title( '', false );
+            } elseif ( is_tag() ) {
+                $title = single_tag_title( '', false );
+            } elseif ( is_author() ) {
+                $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+            } elseif ( is_tax() ) { //for custom post types
+                $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+            } elseif (is_post_type_archive()) {
+                $title = post_type_archive_title( '', false );
+            }
+        return $title;
+    });
