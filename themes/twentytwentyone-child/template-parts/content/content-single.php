37b0f34f395e46f0628563a07c9032e870d1c901
dyerduman <hello@dyerdumandesign.ca>
2021-03-26 10:16:32 -0400

    Adding yoast breadcrumbs into post and page templates

--- themes/twentytwentyone-child/template-parts/content/content-single.php
+++ themes/twentytwentyone-child/template-parts/content/content-single.php
@@ -13,11 +13,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header alignwide">
+		<?php
+	if ( function_exists('yoast_breadcrumb') ) {
+	  yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+	}
+	?>
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twenty_twenty_one_post_thumbnail(); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+
 		<?php
 		the_content();
 
