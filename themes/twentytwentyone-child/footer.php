242972193b03860de34a6a974072bb7d7f28c253
dyerduman <hello@dyerdumandesign.ca>
2021-06-10 12:06:18 -0400

    Update footer.php

--- themes/twentytwentyone-child/footer.php
+++ themes/twentytwentyone-child/footer.php
@@ -96,52 +96,24 @@
 <?php wp_footer(); ?>
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<!-- force zoom
+<!-- smooth scroll polyfill for Safari mobile
+<script defer src="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js"></script>-->
+
+<!-- smooth scroll animation script-->
 <script>
-document.body.style.zoom = 1;
-var scale = 'scale(1)';
-document.body.style.webkitTransform =  scale;     // Chrome, Opera, Safari
- document.body.style.msTransform =   .9;       // IE 9
- document.body.style.transform = scale;     // General
- </script>-->
-<!-- smooth scroll animation script -->
-<script>
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - 100 //minus height of .super-header plus some white space
-        }, 950, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+	var href = $.attr(this, 'href');
+
+	$root.animate({
+			scrollTop: $(href).offset().top
+	}, 500, function () {
+			window.location.hash = href;
+	});
+
+	return false;
+});
 </script>
 
 <!--viewport css animation script-->
